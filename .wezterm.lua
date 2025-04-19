8177e331ace07e2494c41bfa2b2336ddf5204d40
gsuke <54441258+gsuke@users.noreply.github.com>
2025-04-19 13:51:23 +0900

    feat: scroll

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,34 +9,30 @@ local is_linux = wezterm.target_triple == 'x86_64-unknown-linux-gnu'
 -- Font
 config.font = wezterm.font('Myrica M')
 config.font_size = 13
-config.warn_about_missing_glyphs = false
 
 -- Window
 config.window_close_confirmation = 'NeverPrompt'
 config.initial_cols = 90
 config.initial_rows = 30
-
 config.front_end = 'Software'
 
+config.keys = {
+    { mods = 'CTRL|SHIFT', key = 'w',         action = act { CloseCurrentTab = { confirm = false } } }, -- タブを閉じる
+    { mods = 'CTRL|SHIFT', key = 'UpArrow',   action = act.ScrollByLine(-2) },                          -- 上スクロール
+    { mods = 'CTRL|SHIFT', key = 'DownArrow', action = act.ScrollByLine(2) },                           -- 下スクロール
+}
+
 if is_linux then
-    config.keys = {
-        { mods = 'CTRL|SHIFT', key = 't', action = act.SpawnTab 'CurrentPaneDomain' },
-        { mods = 'CTRL|SHIFT', key = 'w', action = act { CloseCurrentTab = { confirm = false } } }
-    }
+    table.insert(config.keys, { mods = 'CTRL|SHIFT', key = 't', action = act.SpawnTab 'CurrentPaneDomain' })
 end
 
 if is_windows then
-    config.keys = {
-        { mods = 'CTRL|SHIFT', key = 't', action = act.ShowLauncher },
-        { mods = 'CTRL|SHIFT', key = 'w', action = act { CloseCurrentTab = { confirm = false } } }
-    }
-
-    local gitbash_path = 'C:/Program Files/Git/bin/bash.exe'
+    table.insert(config.keys, { mods = 'CTRL|SHIFT', key = 't', action = act.ShowLauncher })
 
     config.default_prog = { 'pwsh', '-l' }
     config.launch_menu = {
         { label = 'PowerShell (Core)', args = { 'pwsh' } },
-        { label = 'Git Bash',          args = { gitbash_path } },
+        { label = 'Git Bash',          args = { 'C:/Program Files/Git/bin/bash.exe' } },
         { label = 'Command Prompt',    args = { 'cmd' } },
         { label = 'WSL',               args = { 'wsl' } }
     }
--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
 #requires –RunAsAdministrator
 
+# TODO: multiple file support
 New-Item -Force -ItemType HardLink -Path $HOME\.wezterm.lua -Value .wezterm.lua
