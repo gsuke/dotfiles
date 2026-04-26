5396b0d3b23b801583e1f57ffecdf9fd4285fee4
gsuke <54441258+gsuke@users.noreply.github.com>
2026-04-26 21:48:52 +0900

    format

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,20 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 local act = wezterm.action
 local c = wezterm.config_builder()
 
 -- Font
-local font = 'Myrica M'
-c.font = wezterm.font(font)
+c.font = wezterm.font("Myrica M")
 c.font_size = 18
-c.window_frame = {
-    font = wezterm.font(font),
-    font_size = 16
-}
 
 -- Window
 c.initial_cols = 90
 c.initial_rows = 30
-c.window_close_confirmation = 'NeverPrompt'
+c.window_close_confirmation = "NeverPrompt"
 c.use_ime = true
 
 -- Decoration
@@ -22,24 +17,24 @@ c.default_cursor_style = "BlinkingBar"
 c.window_background_opacity = 0.85
 
 c.keys = {
-    { mods = 'CTRL|SHIFT', key = 't',         action = act.SpawnTab('CurrentPaneDomain') },
-    { mods = 'CTRL|SHIFT', key = 'w',         action = act.CloseCurrentTab({ confirm = false }) },
-    { mods = 'CTRL|SHIFT', key = 'UpArrow',   action = act.ScrollByLine(-2) },
-    { mods = 'CTRL|SHIFT', key = 'DownArrow', action = act.ScrollByLine(2) },
+    { mods = "CTRL|SHIFT", key = "t",         action = act.SpawnTab("CurrentPaneDomain") },
+    { mods = "CTRL|SHIFT", key = "w",         action = act.CloseCurrentTab({ confirm = false }) },
+    { mods = "CTRL|SHIFT", key = "UpArrow",   action = act.ScrollByLine(-2) },
+    { mods = "CTRL|SHIFT", key = "DownArrow", action = act.ScrollByLine(2) },
 
     -- WSLで~が入力されてしまう問題に対処
-    { mods = '',           key = 'F14',       action = act.SendString('') },
+    { mods = "",           key = "F14",       action = act.SendString("") },
 }
 
 -- Linux
 c.enable_wayland = false -- https://github.com/wezterm/wezterm/issues/5103
 
 -- Windows
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    c.default_prog = { 'wsl' }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    c.default_prog = { "wsl" }
     table.insert(
         c.keys,
-        { mods = 'CTRL|SHIFT', key = 'p', action = act.SpawnCommandInNewTab({ args = { 'pwsh' } }) }
+        { mods = "CTRL|SHIFT", key = "p", action = act.SpawnCommandInNewTab({ args = { "pwsh" } }) }
     )
 end
 
